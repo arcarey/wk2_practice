afa2fd865ffc8ed5c5766b3136eedfaacda0cc17
Andrew Carey <andrew.r.carey@gmail.com>
2022-09-25 09:21:37 -0500

    created practice HTML and JS files and linked them

---
+++ variables.js
@@ -0,0 +1,15 @@
+console.log('js');
+
+let testVar = 9;
+
+console.log(testVar);
+
+console.log('the value of testVar is', testVar);
+
+testVar = 02384572
+
+console.log('the value of testVar is', testVar);
+
+testVar = 'new type of variable';
+
+console.log (testVar);
