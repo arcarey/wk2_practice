cd22bbb5bd9c79d4781c18709366ce633aedb9e4
Andrew Carey <andrew.r.carey@gmail.com>
2022-09-25 17:25:24 -0500

    updated logging practice on numbers.js

--- numbers.js
+++ numbers.js
@@ -1,1 +1,12 @@
 console.log('numbersJS')
+let motorcycles = 9;
+let cars = 0;
+
+
+console.log('bikes', motorcycles);
+console.log( "had to put the soft tail in the garage");
+
+//decrement motorcycles
+motorcycles --;
+console.log('bikes', motorcycles);
+
