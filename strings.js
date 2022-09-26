ab0bf9b48bd2e60a229243c73eb78c7a97cbd993
Andrew Carey <andrew.r.carey@gmail.com>
2022-09-26 13:27:02 -0500

    strings practice

--- strings.js
+++ strings.js
@@ -1,1 +1,15 @@
 console.log ('stringsJS');
+
+let catName = 'squiggle';
+let catAge = 3
+console.log(catName);
+
+//comma adds only in console
+console.log ('The name of the cat is' , catName);
+console.log('The cat\'s age is', catAge);
+
+//adding not in console
+let sentense = 'The cat\'s name is ' + catName;
+console.log (sentense)
+
+
